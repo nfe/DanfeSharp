5679f380cc2f92d59ba14fb15360d99dbdfb9450
Michel Balthazar <michel@nfe.io>
2018-11-27 19:49:01 -0200

    Implement Generate QrCode

---
+++ DanfeSharp/GerarQrCode.cs
@@ -0,0 +1,24 @@
+﻿using QRCoder;
+using System.Drawing;
+
+namespace DanfeSharp
+{
+    public class GerarQrCode
+    {
+        public static Bitmap GerarQRCode(string text)
+        {
+            try
+            {
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(plainText: text, eccLevel: QRCodeGenerator.ECCLevel.M, eciMode: QRCodeGenerator.EciMode.Iso8859_1);
+                QRCode qrCode = new QRCode(qrCodeData);
+                return qrCode.GetGraphic(20);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
